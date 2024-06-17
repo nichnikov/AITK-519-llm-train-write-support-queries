00536bc3ec2309033e8196b6c279fe5e11c2ed5d
nichnikov <a.nichnikov@gmail.com>
2024-06-17 18:22:31 +0300

    ref

--- llm_train.py
+++ llm_train.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 import pandas as pd
 import matplotlib.pyplot as plt 
 
@@ -6,13 +6,25 @@ import matplotlib.pyplot as plt
 df = pd.read_feather(os.path.join(os.getcwd(), "data_light", "queries_2020_0.feather"))
 df["QueryLen"] = df["QueryText"].apply(lambda x: len(str(x).split()))
 df["AnswerLen"] = df["Answer"].apply(lambda x: len(str(x).split()))
-print(df)
-print(df[["QueryText", "Answer", "QueryLen", "AnswerLen"]][:20])
-print(df.info())
-
-print(max(df["QueryLen"]))
-print(max(df["AnswerLen"]))
 
 
-df[:1000].plot.bar(rot=0)
-# df["AnswerLen"].plot.bar()
+"""
+QueryLen Median = 48.0
+QueryLen Mean = 60.662
+
+AnswerLen Median = 134.0
+AnswerLen Mean = 166.95275
+"""
+
+df_train = df[(df["QueryLen"] <= 100) & (df["AnswerLen"] <= 250)]
+ptns = re.compile("\n|&nbsp")
+print("patterns:", ptns)
+
+#вопрос\nнаша
+patterns = re.compile(r"\\n|\n|&nbsp;|\\t|\t|&laquo;|\s+")
+
+df_train["Answer"] = df_train["Answer"].apply(lambda x: patterns.sub(" ", x))
+df_train["Answer"] = df_train["Answer"].apply(lambda x: re.sub(r"\s+", " ", x))
+df_train["QueryText"] = df_train["QueryText"].apply(lambda x: patterns.sub(" ", x))
+
+print(df_train[["QueryText", "Answer"]][:10].to_dict(orient="records"))
